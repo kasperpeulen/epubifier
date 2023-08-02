4db955d4f105711f1c4b1791f15d0f317d1d40b3
Kasper Peulen <kasperpeulen@gmail.com>
2023-08-02 08:55:14 +0200

    Get expanded property and all text nodes
    Signed-off-by: Kasper Peulen <kasperpeulen@gmail.com>

--- packages/epubifier/test/pnpm.io.test.ts
+++ packages/epubifier/test/pnpm.io.test.ts
@@ -2,6 +2,7 @@ import { test, expect } from "vitest";
 import { unified } from "unified";
 import rehypeParse from "rehype-parse";
 import { selectAll } from "hast-util-select";
+import { toText } from "hast-util-to-text";
 
 test("get links from page", async () => {
   const html = await fetchHtml("https://pnpm.io/motivation");
@@ -11,7 +12,11 @@ test("get links from page", async () => {
   const elements = selectAll("nav:has(a), [role=navigation]:has(a)", tree);
   const links = elements.map((node) => ({
     label: node.properties?.ariaLabel,
-    links: selectAll("a", node).map(({ properties }) => properties?.href),
+    links: selectAll("a", node).map(({ properties, children }) => ({
+      text: children.map((it) => toText(it)).join(" "),
+      expanded: properties?.ariaExpanded,
+      href: properties?.href,
+    })),
   }));
 
   expect(links).toMatchInlineSnapshot(`
@@ -19,58 +24,256 @@ test("get links from page", async () => {
       {
         "label": "Main",
         "links": [
-          "/",
-          "https://war.ukraine.ua/support-ukraine/",
-          "/motivation",
-          "/blog",
-          "/faq",
-          "/benchmarks",
-          "/community/articles",
-          "/blog",
-          "/faq",
-          "/community/articles",
-          "/benchmarks",
-          "/next/motivation",
-          "/motivation",
-          "/7.x/motivation",
-          "/motivation",
-          "/it/motivation",
-          "/zh/motivation",
-          "/ja/motivation",
-          "/ko/motivation",
-          "/pt/motivation",
-          "/zh-TW/motivation",
-          "/ru/motivation",
-          "/fr/motivation",
-          "/tr/motivation",
-          "/es/motivation",
-          "/id/motivation",
-          "https://translate.pnpm.io",
-          "https://opencollective.com/pnpm",
-          "https://github.com/sponsors/pnpm",
-          "/crypto-donations",
-          "https://github.com/pnpm/pnpm",
+          {
+            "expanded": undefined,
+            "href": "/",
+            "text": " pnpm",
+          },
+          {
+            "expanded": undefined,
+            "href": "https://war.ukraine.ua/support-ukraine/",
+            "text": "",
+          },
+          {
+            "expanded": undefined,
+            "href": "/motivation",
+            "text": "Docs",
+          },
+          {
+            "expanded": undefined,
+            "href": "/blog",
+            "text": "Blog",
+          },
+          {
+            "expanded": undefined,
+            "href": "/faq",
+            "text": "FAQ",
+          },
+          {
+            "expanded": undefined,
+            "href": "/benchmarks",
+            "text": "Benchmarks",
+          },
+          {
+            "expanded": undefined,
+            "href": "/community/articles",
+            "text": "Community",
+          },
+          {
+            "expanded": "false",
+            "href": "#",
+            "text": "More",
+          },
+          {
+            "expanded": undefined,
+            "href": "/blog",
+            "text": "Blog",
+          },
+          {
+            "expanded": undefined,
+            "href": "/faq",
+            "text": "FAQ",
+          },
+          {
+            "expanded": undefined,
+            "href": "/community/articles",
+            "text": "Community",
+          },
+          {
+            "expanded": undefined,
+            "href": "/benchmarks",
+            "text": "Benchmarks",
+          },
+          {
+            "expanded": "false",
+            "href": "/motivation",
+            "text": "8.x",
+          },
+          {
+            "expanded": undefined,
+            "href": "/next/motivation",
+            "text": "Next",
+          },
+          {
+            "expanded": undefined,
+            "href": "/motivation",
+            "text": "8.x",
+          },
+          {
+            "expanded": undefined,
+            "href": "/7.x/motivation",
+            "text": "7.x",
+          },
+          {
+            "expanded": "false",
+            "href": "#",
+            "text": " English",
+          },
+          {
+            "expanded": undefined,
+            "href": "/motivation",
+            "text": "English",
+          },
+          {
+            "expanded": undefined,
+            "href": "/it/motivation",
+            "text": "Italiano (62%)",
+          },
+          {
+            "expanded": undefined,
+            "href": "/zh/motivation",
+            "text": "简体中文 (83%)",
+          },
+          {
+            "expanded": undefined,
+            "href": "/ja/motivation",
+            "text": "日本語 (86%)",
+          },
+          {
+            "expanded": undefined,
+            "href": "/ko/motivation",
+            "text": "한국어 (64%)",
+          },
+          {
+            "expanded": undefined,
+            "href": "/pt/motivation",
+            "text": "Português Brasileiro (55%)",
+          },
+          {
+            "expanded": undefined,
+            "href": "/zh-TW/motivation",
+            "text": "正體中文 (43%)",
+          },
+          {
+            "expanded": undefined,
+            "href": "/ru/motivation",
+            "text": "Русский (43%)",
+          },
+          {
+            "expanded": undefined,
+            "href": "/fr/motivation",
+            "text": "Français (47%)",
+          },
+          {
+            "expanded": undefined,
+            "href": "/tr/motivation",
+            "text": "Türkçe (27%)",
+          },
+          {
+            "expanded": undefined,
+            "href": "/es/motivation",
+            "text": "Español (57%)",
+          },
+          {
+            "expanded": undefined,
+            "href": "/id/motivation",
+            "text": "Bahasa Indonesia (27%)",
+          },
+          {
+            "expanded": undefined,
+            "href": "https://translate.pnpm.io",
+            "text": "Help Us Translate",
+          },
+          {
+            "expanded": "false",
+            "href": "#",
+            "text": "🧡 Sponsor Us",
+          },
+          {
+            "expanded": undefined,
+            "href": "https://opencollective.com/pnpm",
+            "text": "Open Collective ",
+          },
+          {
+            "expanded": undefined,
+            "href": "https://github.com/sponsors/pnpm",
+            "text": "GitHub Sponsors ",
+          },
+          {
+            "expanded": undefined,
+            "href": "/crypto-donations",
+            "text": "Crypto Donations",
+          },
+          {
+            "expanded": undefined,
+            "href": "https://github.com/pnpm/pnpm",
+            "text": "",
+          },
         ],
       },
       {
         "label": "Docs sidebar",
         "links": [
-          "/motivation",
-          "/motivation",
-          "/installation",
-          "/feature-comparison",
+          {
+            "expanded": "true",
+            "href": "/motivation",
+            "text": "Introduction",
+          },
+          {
+            "expanded": undefined,
+            "href": "/motivation",
+            "text": "Motivation",
+          },
+          {
+            "expanded": undefined,
+            "href": "/installation",
+            "text": "Installation",
+          },
+          {
+            "expanded": undefined,
+            "href": "/feature-comparison",
+            "text": "Feature Comparison",
+          },
+          {
+            "expanded": "false",
+            "href": "/pnpm-cli",
+            "text": "Usage",
+          },
+          {
+            "expanded": "false",
+            "href": "/cli/add",
+            "text": "CLI commands",
+          },
+          {
+            "expanded": "false",
+            "href": "/package_json",
+            "text": "Configuration",
+          },
+          {
+            "expanded": "false",
+            "href": "/workspaces",
+            "text": "Features",
+          },
+          {
+            "expanded": "false",
+            "href": "/using-changesets",
+            "text": "Recipes",
+          },
+          {
+            "expanded": "false",
+            "href": "/errors",
+            "text": "Advanced",
+          },
         ],
       },
       {
         "label": "Breadcrumbs",
         "links": [
-          "/",
+          {
+            "expanded": undefined,
+            "href": "/",
+            "text": "",
+          },
         ],
       },
       {
         "label": "Docs pages",
         "links": [
-          "/installation",
+          {
+            "expanded": undefined,
+            "href": "/installation",
+            "text": "Next Installation",
+          },
         ],
       },
     ]
